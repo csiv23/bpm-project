72a40e418d3b5cb16d36cb1439894072a70033a2
Cameron Sivo <cameronsivo5600@gmail.com>
2022-07-25 12:49:12 -0400

    Initial commit.

---
+++ docs/site.js
@@ -0,0 +1,9 @@
+var newyork = document.getElementById("new-york");
+var la = document.getElementById("la");
+var detroit = document.getElementById("detroit");
+var houston = document.getElementById("houston");
+
+
+newyork.addEventListener("click", function(){
+    console.log("yo");
+})
